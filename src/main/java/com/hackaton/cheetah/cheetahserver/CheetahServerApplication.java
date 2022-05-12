55da36bbf1a9aaad85fd97d40f6bf5bbd85f7ce3
Rajani <rajanipv908@gmail.com>
2022-05-12 18:29:25 +0530

    Initial commit

---
+++ src/main/java/com/hackaton/cheetah/cheetahserver/CheetahServerApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.cheetah.cheetahserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheetahServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheetahServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/cheetah/cheetahserver/CheetahServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.cheetah.cheetahserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheetahServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
